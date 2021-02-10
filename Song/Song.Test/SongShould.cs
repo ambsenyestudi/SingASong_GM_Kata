fceec919970ab01b39e1892b927ee4d6eaba080a
ambsenyestudi <adrifre7@hotmail.com>
2021-02-10 19:30:13 +0100

    started with ApprovalTests.Net

--- Song/Song.Test/SongShould.cs
+++ Song/Song.Test/SongShould.cs
@@ -1,14 +1,16 @@
+using ApprovalTests;
 using System;
 using Xunit;
 
 namespace Song.Test
 {
-    public class UnitTest1
+    public class SongShould
     {
         [Fact]
         public void Test1()
         {
-
+            var sut = new Lyrics();
+            Approvals.Verify(sut.Song);
         }
     }
 }
